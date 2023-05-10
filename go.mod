2984e6040479554491d5a9552cb35942f373ecac
xuanyan <1430392553@qq.com>
2023-05-10 11:16:08 +0800

    跨域的AllowCredentials 解释

--- go.mod
+++ go.mod
@@ -36,6 +36,9 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
--- middleware/cors.go
+++ middleware/cors.go
@@ -10,10 +10,11 @@ func Cors() gin.HandlerFunc {
 	return cors.New(
 		cors.Config{
 			//AllowAllOrigins:  true,
-			AllowOrigins:     []string{"*"}, // 等同于允许所有域名 #AllowAllOrigins:  true
-			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowHeaders:     []string{"*", "Authorization"},
-			ExposeHeaders:    []string{"Content-Length", "text/plain", "Authorization", "Content-Type"},
+			AllowOrigins:  []string{"*"}, // 等同于允许所有域名 #AllowAllOrigins:  true
+			AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowHeaders:  []string{"*", "Authorization"},
+			ExposeHeaders: []string{"Content-Length", "text/plain", "Authorization", "Content-Type"},
+			//跨域请求的资源凭证，被指定可以选择cookie、HTTP签名header等
 			AllowCredentials: true,
 			MaxAge:           12 * time.Hour,
 		},
